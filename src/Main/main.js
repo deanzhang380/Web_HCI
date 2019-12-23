3aa930da5e1dafa034d3073d29f183dd7379acff
NGẠN TRƯƠNG TUẤN <16520796@ms.uit.edu.vn>
2019-12-23 23:57:17 +0700

    Form_top_view create

--- src/Main/main.js
+++ src/Main/main.js
@@ -3,13 +3,17 @@ import { Container, Row, Col } from 'reactstrap';
 import './main.css'
 import Card_info from './Card/card.js'
 import RecomBar from './Recommend_Bar/recommend_bar.js'
+import Form_top_view from './Form_View/Form_top_view.js'
 const Main = () => {;
   return (
     <Container className="App-Container">
         <div className="">
         <RecomBar></RecomBar>
         </div>
-         
+         <div>
+           <Form_top_view>
+           </Form_top_view>
+         </div>
 
     </Container>
   );
